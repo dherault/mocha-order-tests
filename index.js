808210d705dcc76ef125875140dd6bd85ffd30a3
David Hérault <dherault@gmail.com>
2020-01-22 16:00:39 +0100

    v0.0.2

--- index.js
+++ index.js
@@ -1,17 +1,39 @@
 'use strict';
 
+const fs = require('fs');
+const path = require('path');
 const Mocha = require('mocha');
+const micromatch = require('micromatch');
 
 const run = Mocha.prototype.run;
 const each = Mocha.Suite.prototype.eachTest;
 
 Mocha.prototype.run = function () {
-    console.log(this.files);
-    return run.apply(this, arguments);
+  this.files = sortFiles(this.files);
+
+  return run.apply(this, arguments);
 };
 
-// Mocha.Suite.prototype.eachTest = function () {
-//     shuffle(this.tests);
-//     shuffle(this.suites);
-//     return each.apply(this, arguments);
-// };
+function sortFiles(fileNames) {
+  const testsOrderJsonFilePath = path.resolve(__dirname, '../../.tests-order.json');
+
+  if (!fs.existsSync(testsOrderJsonFilePath)) {
+    console.log('Error loading .tests-order.json file : file not found');
+
+    return fileNames;
+  }
+
+  const testsOrderJsonFileContent = fs.readFileSync(testsOrderJsonFilePath, 'uft-8');
+  let testsOrder;
+
+  try {
+    testsOrder = JSON.parse(testsOrderJsonFileContent);
+  }
+  catch (error) {
+    console.log('Error loading .tests-order.json file : invalid JSON');
+
+    return fileNames;
+  }
+
+  console.log(testOrder);
+}
